e21a339ba87750b261855e6ca10269da220df82d
Rustam Rasimovich <horrorvanchugov@gmail.com>
2023-11-25 13:39:36 +0300

    = update config for G++ compiler

--- out/templates/project/g++/build.bat
+++ out/templates/project/g++/build.bat
@@ -1,16 +1,20 @@
 @echo off
 set CXX=g++
 set CXX_FLAGS=-std=c++23 -Ofast -static-libstdc++ -static-libgcc
+set LIB_FLAGS=
 set BIN=bin
 set SRC=src
 set INCLUDE=include\
-set LIB=
+set LIB=lib
+@REM place your libraries names here
+set LIBS=
 set EXECUTABLE=main
 
 if not exist %BIN% mkdir %BIN%
 
 echo Compiling the C++ program...
-%CXX% %CXX_FLAGS% -I%INCLUDE% %SRC%\*.cpp -o %BIN%\%EXECUTABLE% %LIB%
+for /f "tokens=*" %%i in ('pkg-config --cflags --libs %LIBS%') do set CXX_FLAGS=%CXX_FLAGS% %%i
+%CXX% %CXX_FLAGS% -I%INCLUDE% -L%LIB% %SRC%\*.cpp -o %BIN%\%EXECUTABLE% %LIB_FLAGS%
 if %errorlevel% neq 0 (
     echo Compilation failed.
     exit /b %errorlevel%
